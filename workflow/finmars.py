163b183a074399bbaaa996fbe95920cef6682173
Sergey Zhitenev <szhitenev@ip-192-168-178-21.eu-central-1.compute.internal>
2023-01-28 16:57:56 +0100

    update finmars-workflow api interface

--- workflow/finmars.py
+++ workflow/finmars.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 import requests
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -6,19 +7,18 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from workflow.models import User
 from workflow_app import settings
 
-
-import logging
 _l = logging.getLogger('workflow')
 
-def execute_expression(expression):
 
+def execute_expression(expression):
     bot = User.objects.get(username="finmars_bot")
 
     refresh = RefreshToken.for_user(bot)
 
     # _l.info('refresh %s' % refresh.access_token)
 
-    headers = {'Content-type': 'application/json', 'Accept': 'application/json', 'Authorization': 'Bearer %s' % refresh.access_token}
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json',
+               'Authorization': 'Bearer %s' % refresh.access_token}
     data = {
         'expression': expression,
         'is_eval': True
@@ -28,109 +28,153 @@ def execute_expression(expression):
 
     response = requests.post(url=url, data=json.dumps(data), headers=headers)
 
+    if response.status_code != 200:
+        raise Exception(response.text)
+
     return response.json()
 
 
 def execute_expression_procedure(payload):
-
     bot = User.objects.get(username="finmars_bot")
 
     refresh = RefreshToken.for_user(bot)
 
     # _l.info('refresh %s' % refresh.access_token)
 
-    headers = {'Content-type': 'application/json', 'Accept': 'application/json', 'Authorization': 'Bearer %s' % refresh.access_token}
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json',
+               'Authorization': 'Bearer %s' % refresh.access_token}
     data = payload
 
     url = settings.HOST_URL + '/' + settings.BASE_API_URL + '/api/v1/procedures/expression-procedure/execute/'
 
     response = requests.post(url=url, data=json.dumps(data), headers=headers)
 
+    if response.status_code != 200:
+        raise Exception(response.text)
+
     return response.json()
 
 
 def execute_data_procedure(payload):
-
     bot = User.objects.get(username="finmars_bot")
 
     refresh = RefreshToken.for_user(bot)
 
     # _l.info('refresh %s' % refresh.access_token)
 
-    headers = {'Content-type': 'application/json', 'Accept': 'application/json', 'Authorization': 'Bearer %s' % refresh.access_token}
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json',
+               'Authorization': 'Bearer %s' % refresh.access_token}
     data = payload
 
     url = settings.HOST_URL + '/' + settings.BASE_API_URL + '/api/v1/procedures/data-procedure/execute/'
 
     response = requests.post(url=url, data=json.dumps(data), headers=headers)
 
+    if response.status_code != 200:
+        raise Exception(response.text)
+
     return response.json()
 
 
 def execute_pricing_procedure(payload):
-
     bot = User.objects.get(username="finmars_bot")
 
     refresh = RefreshToken.for_user(bot)
 
     # _l.info('refresh %s' % refresh.access_token)
 
-    headers = {'Content-type': 'application/json', 'Accept': 'application/json', 'Authorization': 'Bearer %s' % refresh.access_token}
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json',
+               'Authorization': 'Bearer %s' % refresh.access_token}
     data = payload
 
     url = settings.HOST_URL + '/' + settings.BASE_API_URL + '/api/v1/procedures/pricing-procedure/execute/'
 
     response = requests.post(url=url, data=json.dumps(data), headers=headers)
 
+    if response.status_code != 200:
+        raise Exception(response.text)
+
     return response.json()
 
-def execute_task(payload):
 
+def execute_task(payload):
     bot = User.objects.get(username="finmars_bot")
 
     refresh = RefreshToken.for_user(bot)
 
     # _l.info('refresh %s' % refresh.access_token)
 
-    headers = {'Content-type': 'application/json', 'Accept': 'application/json', 'Authorization': 'Bearer %s' % refresh.access_token}
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json',
+               'Authorization': 'Bearer %s' % refresh.access_token}
     data = payload
 
-    url = settings.HOST_URL + '/' + settings.BASE_API_URL + '/api/v1/tasks/execute/'
+    url = settings.HOST_URL + '/' + settings.BASE_API_URL + '/api/v1/tasks/task/execute/'
 
     response = requests.post(url=url, data=json.dumps(data), headers=headers)
 
+    if response.status_code != 200:
+        raise Exception(response.text)
+
     return response.json()
 
-def execute_transaction_import(payload):
 
+def get_task(id):
     bot = User.objects.get(username="finmars_bot")
 
     refresh = RefreshToken.for_user(bot)
 
     # _l.info('refresh %s' % refresh.access_token)
 
-    headers = {'Content-type': 'application/json', 'Accept': 'application/json', 'Authorization': 'Bearer %s' % refresh.access_token}
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json',
+               'Authorization': 'Bearer %s' % refresh.access_token}
+
+    url = settings.HOST_URL + '/' + settings.BASE_API_URL + '/api/v1/tasks/task/%s/' % id
+
+    response = requests.get(url=url, headers=headers)
+
+    if response.status_code != 200:
+        raise Exception(response.text)
+
+    return response.json()
+
+
+def execute_transaction_import(payload):
+    bot = User.objects.get(username="finmars_bot")
+
+    refresh = RefreshToken.for_user(bot)
+
+    # _l.info('refresh %s' % refresh.access_token)
+
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json',
+               'Authorization': 'Bearer %s' % refresh.access_token}
     data = payload
 
     url = settings.HOST_URL + '/' + settings.BASE_API_URL + '/api/v1/import/transaction-import/execute/'
 
     response = requests.post(url=url, data=json.dumps(data), headers=headers)
 
+    if response.status_code != 200:
+        raise Exception(response.text)
+
     return response.json()
 
-def execute_simple_import(payload):
 
+def execute_simple_import(payload):
     bot = User.objects.get(username="finmars_bot")
 
     refresh = RefreshToken.for_user(bot)
 
     # _l.info('refresh %s' % refresh.access_token)
 
-    headers = {'Content-type': 'application/json', 'Accept': 'application/json', 'Authorization': 'Bearer %s' % refresh.access_token}
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json',
+               'Authorization': 'Bearer %s' % refresh.access_token}
     data = payload
 
     url = settings.HOST_URL + '/' + settings.BASE_API_URL + '/api/v1/import/simple-import/execute/'
 
     response = requests.post(url=url, data=json.dumps(data), headers=headers)
+
+    if response.status_code != 200:
+        raise Exception(response.text)
 
     return response.json()
