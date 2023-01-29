02766445a7c983a612f50526bca540373fe86a4e
Sergey Zhitenev <szhitenev@ip-192-168-178-21.eu-central-1.compute.internal>
2023-01-29 01:20:30 +0100

    update context processor

--- workflow/context_processors.py
+++ workflow/context_processors.py
@@ -2,4 +2,4 @@ from django.conf import settings # import the settings file
 
 def workflow(request):
     # return the value you want as a dictionnary. you may add multiple values in there.
-    return {'FLOWER_URL': settings.FLOWER_URL}
+    return {'FLOWER_URL': settings.FLOWER_URL, 'BASE_API_URL': settings.BASE_API_URL}
