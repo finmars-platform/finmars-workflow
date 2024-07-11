ad2bf87f24984f93c222a33f8a99c83aa01a201c
Anton Alekseev <toxa23@gmail.com>
2024-07-11 09:42:22 +0300

    create JWT tokens with username

--- workflow_app/settings.py
+++ workflow_app/settings.py
@@ -488,8 +488,8 @@ SIMPLE_JWT = {
 
     'AUTH_HEADER_TYPES': ('Bearer'),
     'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
-    'USER_ID_FIELD': 'id',
-    'USER_ID_CLAIM': 'user_id',
+    'USER_ID_FIELD': 'username',
+    'USER_ID_CLAIM': 'username',
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
     'TOKEN_TYPE_CLAIM': 'token_type',
 
